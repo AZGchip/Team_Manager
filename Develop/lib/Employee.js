3350d2f1d096d2cd5c0d98b22eebdd454f0f09e7
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-04-13 15:35:37 -0400

    installed testing and basic structure prefab provided to the class. added employee construstor class.

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,20 @@
+// To Do: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        const name = name;
+        const id = id;
+        const email = email;
+    }
+    getName(){
+
+    }
+    getId(){
+
+    }
+    getEmail(){
+
+    }
+    getRole(){
+        return "Employee"
+    }
+}
